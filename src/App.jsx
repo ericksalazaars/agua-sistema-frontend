a84a20af35aaaf769695ef433a17c1402d2d8c9f
Erick Salazar <erick.salazaars@gmail.com>
2025-11-11 22:19:52 -0600

    fix app.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ClientesPage from "./pages/ClientesPage";
 import VisitasPage from "./pages/VisitasPage";
 
-
 export default function App() {
-return (
-<BrowserRouter>
-<Routes>
-<Route path="/" element={<ClientesPage />} />
-<Route path="/visitas" element={<VisitasPage />} />
-</Routes>
-</BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClientesPage />} />
+        <Route path="/visitas" element={<VisitasPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
